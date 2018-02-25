7a456e653fd5ed6d94b6d8ff1422a409de464a82
Marco Dell'Olio <marcodellolio@Marcos-MacBook-Pro.local>
2018-02-25 12:59:05 -0500

    clicking on the recipe titel the user can see more recipe details

--- client/src/components/CookBookRecipe.js
+++ client/src/components/CookBookRecipe.js
@@ -5,18 +5,55 @@ import styled from 'styled-components'
 
 class CookbookRecipe extends Component {
 
+    state = { 
+        selected : false
+    }
+
+    selectRecipe = () => {
+        !this.state.selected? 
+            this.setState({selected : true})
+        :
+            this.setState({selected : false})
+    }
 
     render() {
 
         const recipe = this.props
         
         return (
-            <div>
-                {recipe.title}
-            </div>
+            <Recipe >
+                <RecipeBar>
+                    <div onClick={() => this.selectRecipe()}>{recipe.title}</div>
+                    {
+                        !this.state.selected? null :
+                        
+                        <div>
+                            <Img src={recipe.image} alt=""/> 
+                        </div>
+                    }        
+                </RecipeBar>
+                
+                
+            </Recipe>
 
         )
     }
   }
   
   export default CookbookRecipe;
+
+  const Recipe = styled.div`
+    width : 100%;
+    height : auto;
+    margin : 2%;
+    background-color : green
+  `
+
+  const RecipeBar = styled.div`
+    width : 100%
+  `
+
+  const RecipeContent = styled.div`
+    width : 100%;
+    height : auto;
+  `
